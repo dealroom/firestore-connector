c235596c5cb2a4b44182874ced3ff6b4e793b906
saltas888 <d.saltaferis@gmail.com>
2020-10-20 10:50:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dealroom-firestore-connector",  # Replace with your own username
-    version="0.0.3",
+    version="0.0.5",
     author="Dealroom.co",
     author_email="data@dealroom.co",
     description="A wrapper class for accessing Google Cloud Firestore.",
