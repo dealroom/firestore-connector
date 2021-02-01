5534c37f96d80d6c7d70eb18be957b8119975bed
saltas888 <d.saltaferis@gmail.com>
2021-02-02 00:34:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,10 +20,7 @@ setuptools.setup(
     ],
     install_requires=[
         "google-cloud-firestore",
-        "dealroom_urlextract @ https://github.com/dealroom/data-firestore-connector/tree/set-history-url#egg=dealroom_urlextract",
-    ],
-    dependency_links=[
-        "https://github.com/dealroom/data-firestore-connector/tree/set-history-url#egg=dealroom_urlextract"
+        "dealroom_urlextract @ git+https://github.com/dealroom/data-firestore-connector/tree/set-history-url#egg=dealroom_urlextract",
     ],
     python_requires=">=3.6",
 )
