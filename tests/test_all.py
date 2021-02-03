a86df2417ffc383e6a6e5f3014bf0cad5ed5cd3c
saltas888 <d.saltaferis@gmail.com>
2021-02-03 17:21:20 +0100

    Document tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,6 @@
 import random
 import string
+from random import randint
 
 import dealroom_firestore_connector as fc
 import pytest
@@ -18,6 +19,7 @@ def test_collection_exists():
 
 
 def test_set_history_doc_refs_empty_final_url():
+    """Creating a new document, without a final_url should raise an error"""
     db = fc.new_connection(project=TEST_PROJECT) 
     # TODO: Use it as soon as firestore-connector will raise a proper Error
     # with pytest.raises(KeyError, match=r"'final_url'"):
@@ -27,6 +29,7 @@ def test_set_history_doc_refs_empty_final_url():
     assert res == ERROR
 
 def test_set_history_doc_refs_wrong_final_url():
+    """Creating a new document, with invalid final_url should raise an error"""
     db = fc.new_connection(project=TEST_PROJECT) 
     # TODO: Use it as soon as firestore-connector will raise a proper Error
     # with pytest.raises(Exception):
@@ -37,7 +40,27 @@ def test_set_history_doc_refs_wrong_final_url():
     assert res == ERROR
 
 
+def test_set_history_doc_refs_new():
+    """Creating a new document, with valid final_url & w/o dealroom id should be ok"""
+    db = fc.new_connection(project=TEST_PROJECT) 
+    res = fc.set_history_doc_refs(db, {
+        "final_url": f"{_get_random_string(10)}.com"
+    })
+
+    assert res == SUCCESS
+
+def test_set_history_doc_refs_new():
+    """Creating a new document, with valid final_url & valid dealroom id should be ok"""
+    db = fc.new_connection(project=TEST_PROJECT) 
+    res = fc.set_history_doc_refs(db, {
+        "final_url": f"{_get_random_string(10)}.com",
+        "dealroom_id": randint(1e5, 1e8)
+    })
+
+    assert res == SUCCESS
+
 def test_set_history_doc_refs_empty_dealroom_id():
+    """Updating a new document, with valid final_url & w/o dealroom id should be ok"""
     db = fc.new_connection(project=TEST_PROJECT) 
     random_field = _get_random_string(10)
     res = fc.set_history_doc_refs(db, {
@@ -48,6 +71,7 @@ def test_set_history_doc_refs_empty_dealroom_id():
     
 
 def test_set_history_doc_refs_wrong_dealroom_id():
+    """Creating/Updating a new document, with invalid dealroomid should raise an error"""
     db = fc.new_connection(project=TEST_PROJECT) 
     # TODO: Use it as soon as firestore-connector will raise a proper Error
     # with pytest.raises(ValueError, match=r"'dealroom_id'"):
@@ -57,20 +81,3 @@ def test_set_history_doc_refs_wrong_dealroom_id():
     })
     assert res == -1
 
-def test_set_history_doc_refs_new():
-    db = fc.new_connection(project=TEST_PROJECT) 
-    res = fc.set_history_doc_refs(db, {
-        "final_url": f"{_get_random_string(10)}.com"
-    })
-
-    assert res == SUCCESS
-
-def test_set_history_doc_refs_update_by_final_url():
-    db = fc.new_connection(project=TEST_PROJECT) 
-    random_field = _get_random_string(10)
-    res = fc.set_history_doc_refs(db, {
-        "final_url": "foo.bar",
-        "test_field": random_field
-    })
-
-    assert res == SUCCESS
