dac1b469510afd52ad22b9c10a046b49c7d3e8e5
saltas888 <d.saltaferis@gmail.com>
2021-02-03 18:04:37 +0100

    Allow empty final_url + dealroom_id = -2 + formatting

--- dealroom_firestore_connector/__init__.py
+++ dealroom_firestore_connector/__init__.py
@@ -136,7 +136,6 @@ def stream(collection_ref, *args, **kwargs):
             return ERROR
 
 
-
 def collection_exists(collection_ref: CollectionReference):
     """A helper method to check whether a collection exists
 
@@ -154,15 +153,18 @@ def collection_exists(collection_ref: CollectionReference):
     docs = get(collection_ref.limit(1))
 
     if docs == -1:
-        logging.error("Couldn't get collection_ref. Please check the logs above for possible errors.")
+        logging.error(
+            "Couldn't get collection_ref. Please check the logs above for possible errors."
+        )
         return False
     return len(docs) > 0
 
+
 HISTORY_COLLECTION_PATH = "history"
 
+
 def get_history_doc_refs(
-    db: firestore.Client,
-    finalurl_or_dealroomid: str
+    db: firestore.Client, finalurl_or_dealroomid: str
 ) -> Union[Tuple[DocumentReference], int]:
     """Returns a DocumentReference based on the final_url field or dealroom_id
     field.
@@ -201,11 +203,26 @@ def get_history_doc_refs(
     return tuple(doc.reference for doc in docs)
 
 
+# We mark the deleted entities from DR database with -2 entity so we can easily identify them.
+_DELETED_DEALROOM_ENTITY_ID = -2
+# We mark the entities that don't exist in DR with -1.
+_NOT_IN_DEALROOM_ENTITY_ID = -1
+
+
 def _validate_dealroomid(dealroom_id: Union[str, int]):
-    if not str(dealroom_id).isnumeric() and dealroom_id != -1:
+    # isnumeric() returns False for negative numbers. Thus, we check it manually.
+    if not str(dealroom_id).isnumeric() and int(dealroom_id) not in [
+        _DELETED_DEALROOM_ENTITY_ID,
+        _NOT_IN_DEALROOM_ENTITY_ID,
+    ]:
         raise ValueError("'dealroom_id' must be an integer")
 
+
 def _validate_final_url(final_url: str):
+    # Empty string for final_url is valid in case that this entity, doesn't use that url anymore.
+    if final_url == "":
+        return
+
     # Extract method has internally validation rules. Check here:
     # https://github.com/dealroom/data-urlextract/blob/main/dealroom_urlextract/__init__.py#L33-L35
     try:
@@ -213,33 +230,33 @@ def _validate_final_url(final_url: str):
     except:
         raise ValueError("'final_url' must have a url-like format")
 
+
 def _validate_new_history_doc_payload(payload: dict):
     """Validate the required fields in the payload when creating a new document"""
 
     if "final_url" not in payload:
         raise KeyError("'final_url' must be present payload")
-    
+
     _validate_final_url(payload["final_url"])
-    
+
     if "dealroom_id" not in payload:
         raise KeyError("'dealroom_id' must be present payload")
 
     _validate_dealroomid(payload["dealroom_id"])
+
 
 def _validate_update_history_doc_payload(payload: dict):
     """Validate the required fields in the payload when updating"""
 
     if "final_url" in payload:
         _validate_final_url(payload["final_url"])
-    
+
     if "dealroom_id" in payload:
         _validate_dealroomid(payload["dealroom_id"])
 
 
 def set_history_doc_refs(
-    db: firestore.Client,
-    payload: dict,
-    finalurl_or_dealroomid: str = None
+    db: firestore.Client, payload: dict, finalurl_or_dealroomid: str = None
 ) -> None:
     """Updates or creates a document in history collection
 
@@ -255,20 +272,25 @@ def set_history_doc_refs(
     """
 
     history_col = db.collection(HISTORY_COLLECTION_PATH)
-    
-    _payload = { **payload }
+
+    _payload = {**payload}
 
     # If finalurl_or_dealroomid is not provided then a new document will be created.
-    history_refs = get_history_doc_refs(db, finalurl_or_dealroomid) if finalurl_or_dealroomid else []
+    history_refs = (
+        get_history_doc_refs(db, finalurl_or_dealroomid)
+        if finalurl_or_dealroomid
+        else []
+    )
 
-    if history_refs == -1:
+    if history_refs == ERROR:
         return ERROR
     # CREATE: If there is not available documents in history
     elif len(history_refs) == 0:
         # Add any default values to the payload
         _payload = {
-            "dealroom_id": -1,
-            **payload
+            "dealroom_id": _NOT_IN_DEALROOM_ENTITY_ID,
+            "final_url": "",
+            **payload,
         }
         # Validate that the new document will have the minimum required fields
         try:
@@ -277,7 +299,7 @@ def set_history_doc_refs(
             logging.error(ex)
             return ERROR
         history_ref = history_col.document()
-  
+
     # UPDATE:
     elif len(history_refs) == 1:
         try:
@@ -289,25 +311,23 @@ def set_history_doc_refs(
         history_ref = history_refs[0]
     # If more than one document were found then it's an error.
     else:
-         # TODO: Raise a Custom Exception (DuplicateDocumentsException) with the same message when we replace ERROR constant with actual exceptions
+        # TODO: Raise a Custom Exception (DuplicateDocumentsException) with the same message when we replace ERROR constant with actual exceptions
         logging.error("Found more than one documents to update for this payload")
         return ERROR
-    
+
     # Ensure that dealroom_id is type of number
     if "dealroom_id" in _payload:
         _payload["dealroom_id"] = int(_payload["dealroom_id"])
     res = set(history_ref, _payload)
-        
-    if res == -1:
+
+    if res == ERROR:
         # TODO: Raise a Custom Exception (FirestoreException) with the same message when we replace ERROR constant with actual exceptions
-        logging.error(f"Couldn't `set` document {finalurl_or_dealroomid}. Please check logs above.")
+        logging.error(
+            f"Couldn't `set` document {finalurl_or_dealroomid}. Please check logs above."
+        )
         return ERROR
 
     return SUCCESS
-   
-
-
-
 
 
 def __log_exception(error_code, ref, identifier, was_retried=False):
